508292853919452a77676df6955caaacede96fa9
Ashutosh Kumar <ashutosh.kumar@precisely.com>
2023-09-26 13:40:49 +0530

    updated   app file

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 require("dotenv").config();
 const app = express();
@@ -8,12 +7,12 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`This is the home page and running on ${process.env.PORT}`);
+    .send(`This is the home page and running on ${process.env.PORT || 8000}`);
 });
 
 // Listening to the port 
-let PORT = process.env.PORT ? process.env.PORT : 8000;
-console.log(PORT);
+const PORT = process.env.PORT || 8000;
+console.log(`Server is running on port ${PORT}`);
 app.listen(PORT, () => {
   console.log(`Server is running...`);
 });
